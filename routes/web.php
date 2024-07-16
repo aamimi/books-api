41473b2af1894c5cfa29b4a728b2b9a4170377cf
Amimi Achraf <31471856+aamimi@users.noreply.github.com>
2024-07-16 01:07:45 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get(
-    '/hello',
-    function(){
-    return 'Hello World';});
+Route::get('/hello', function() {
+    return 'Hello World';
+});
